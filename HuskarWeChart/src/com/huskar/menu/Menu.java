3d22bac3115978de166e935b7c1d4a0decdbf10e
Administrator <Administrator@PC_DIAOWEI.zte.com.cn>
2015-11-17 17:16:49 +0800

    20151117 add menu

---
+++ HuskarWeChart/src/com/huskar/menu/Menu.java
@@ -0,0 +1,17 @@
+package com.huskar.menu;
+
+/**
+ * ²Ëµ¥
+ * 
+ */
+public class Menu {
+	private Button[] button;
+
+	public Button[] getButton() {
+		return button;
+	}
+
+	public void setButton(Button[] button) {
+		this.button = button;
+	}
+}
